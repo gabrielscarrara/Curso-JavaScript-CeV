53f91525449a874cd2524bd695d602450362f30d
gabrielscarrara <gabrielsouzacarrara@hotmail.com>
2023-05-05 10:00:38 -0300

    add exercicios

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 78
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {  // COndição simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
+
+
